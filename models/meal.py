69055c096e577f4f005dd22a0bdc92c4334d50a8
Marcos Tavares <mjtdes.md@gmail.com>
2025-01-23 16:56:47 -0300

    feat: Create initial meal class structure

---
+++ models/meal.py
@@ -0,0 +1,9 @@
+from database import db
+from flask_login import UserMixin
+
+class Meal(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(100), nullable=False)
+    date = db.Column(db.DateTime, nullable=False)
+    isInDiet = db.Column(db.Boolean, nullable=False, default=True)
