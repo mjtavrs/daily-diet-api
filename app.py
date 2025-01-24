25cee26ad0eea0c6900c5f95c60d1d38a74ef9a3
Marcos Tavares <mjtdes.md@gmail.com>
2025-01-24 13:24:04 -0300

    refactor: Refactor the 'user' and 'meal' models, changing the user_id and meal_id attributes back to 'id'

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 from models.user import User
 from models.meal import Meal
 from database import db
-from flask_login import LoginManager, login_user, logout_user
+from flask_login import LoginManager, login_user, current_user, logout_user
 import bcrypt
 
 app = Flask(__name__)
@@ -11,10 +11,33 @@ app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:admin123@127.0.0.1
 
 login_manager = LoginManager()
 db.init_app(app)
+login_manager.init_app(app)
+
+login_manager.login_view = 'login'
 
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(user_id)
+
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.json
+    username = data.get('username')
+    password = data.get('password')
+
+    if username and password:
+        user = User.query.filter_by(username=username).first()
+
+        if user and bcrypt.checkpw(str.encode(password), str.encode(user.password)):
+            login_user(user)
+            return jsonify({"message": "Autenticação realizada"})
+        
+    return jsonify({"message": "Credenciais inválidas"}), 404
+
+@app.route('/logout', methods=['GET'])
+def logout():
+    logout_user()
+    return jsonify({"message": "Usuário deslogado com sucesso"})
 
 @app.route('/user', methods=['POST'])
 def create_user():
@@ -30,23 +53,6 @@ def create_user():
         return jsonify({"message": "Usuário cadastrado com sucesso"})
 
     return jsonify({"message": "Preencha corretamente os dados"}), 400
-
-@app.route('/login', methods=['POST'])
-def login():
-    data = request.json
-    username = data.get('username')
-    password = data.get('password')
-
-    if username and password:
-        user = User.query.filter_by(username=username).first()
-        if user and bcrypt.checkpw(str.encode(password), str.encode(user.password)):
-            login_user(user)
-            return jsonify({"message": "Usuário logado com sucesso"})
-
-@app.route('/logout', methods=['GET'])
-def logout():
-    logout_user()
-    return jsonify({"message": "Usuário deslogado com sucesso"})
 
 # Register a meal
 
--- models/user.py
+++ models/user.py
@@ -2,6 +2,6 @@ from database import db
 from flask_login import UserMixin
 
 class User(db.Model, UserMixin):
-    user_id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), nullable=False)
     password = db.Column(db.String(80), nullable=False)
